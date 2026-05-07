f2aa3ecb0365f5223159208fac337f1c30cd82c6
YassinShehab206 <Y.Ibrahim03610@student.aast.edu>
2026-05-08 00:15:14 +0300

    reloooooooooad

--- include/game/player/weapon/banana.h
+++ include/game/player/weapon/banana.h
@@ -5,10 +5,11 @@ class Banana : public Weapon
 {
 public:
     Banana()
-        : Weapon(30.0f, 1000.0f, 0.1f, 5.0f, 30, 2.0f, "res/art/banana.png", 4)
+        : Weapon(30.0f, 1000.0f, 0.1f, 5.0f, 30, 1.0f, "res/art/banana.png", 4)
     {
         anim_idle = { {0}, 1.0f };
-        anim_fire = { {1, 2, 3}, 0.2f };
+        anim_fire = { {0, 1}, 0.05f };
+        anim_reload = { {1, 2, 3}, 0.2f };
 
         current_anim = &anim_idle;
     }
